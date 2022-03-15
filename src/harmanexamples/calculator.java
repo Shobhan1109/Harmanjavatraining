93ee7b83756f7b63e960854f8cb0abd9254b35f5
Shobhan S <shobhan.11.2000@gmail.com>
2022-03-15 15:52:28 +0530

    Addition operation with function is completed

--- src/harmanexamples/calculator.java
+++ src/harmanexamples/calculator.java
@@ -3,14 +3,21 @@ package harmanexamples;
 import java.util.Scanner;
 
 public class calculator {
+    public int AdditionOperation(int x, int y)
+    {
+        int z=x+y;
+        return z;
+    }
     public static void main(String[] args) {
+
+        calculator calc=new calculator();
         Scanner input = new Scanner(System.in);
         int n1,n2,result;
         System.out.println("enter the n1:");
         n1=input.nextInt();
         System.out.println("enter the n2:");
         n2=input.nextInt();
-        result=n1+n2;
+        result= calc.AdditionOperation(n1,n2);
         System.out.println("Result is "+result);
     }
 }
