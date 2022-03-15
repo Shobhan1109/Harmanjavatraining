9b88866c3d17520bf980adc0e2628e08708d0c00
Shobhan S <shobhan.11.2000@gmail.com>
2022-03-15 17:07:44 +0530

    Calculator is updates using inheritence concept

---
+++ src/harmanexamples/Parentcalculator.java
@@ -0,0 +1,16 @@
+package harmanexamples;
+
+public class Parentcalculator {
+    public int AddOperation(int x, int y) {
+        return x + y;
+    }
+    public int SubOperation(int x, int y) {
+        return x - y;
+    }
+    public int MulOperation(int x, int y) {
+        return x*y;
+    }
+    public int DivOperation(int x, int y) {
+        return x/y;
+    }
+}
--- src/harmanexamples/calculator.java
+++ src/harmanexamples/calculator.java
@@ -2,13 +2,10 @@ package harmanexamples;
 
 import java.util.Scanner;
 
-public class calculator {
+public class calculator extends Parentcalculator{
 
     public static void main(String[] args) {
-        Addition addcalc=new Addition();
-        Multiplication mulcalc=new Multiplication();
-        Division divcalc=new Division();
-        Subtraction subcalc=new Subtraction();
+        calculator obj=new calculator();
 
         Scanner input = new Scanner(System.in);
         int n1,n2,result1, result2, result3, result4;
@@ -16,10 +13,10 @@ public class calculator {
         n1=input.nextInt();
         System.out.println("enter the n2:");
         n2=input.nextInt();
-        result1= addcalc.AdditionOperation(n1,n2);
-        result2= mulcalc.MultiplicationOperation(n1,n2);
-        result3= divcalc.DivisionOperation(n1,n2);
-        result4= subcalc.SubtractionOperation(n1,n2);
+        result1= obj.AddOperation(n1,n2);
+        result2= obj.DivOperation(n1,n2);
+        result3= obj.MulOperation(n1,n2);
+        result4= obj.SubOperation(n1,n2);
         System.out.println("Addition: "+result1);
         System.out.println("Multiplication: "+result2);
         System.out.println("Division: "+result3);
